d58dd8037d9e3b686fdebe81eaf7283ff2df183e
dennis03 <dennisleung03@gmail.com>
2021-09-25 22:48:48 +0800

    parasite

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -34,10 +34,75 @@ def parasite():
             ans01['p3'] = int(np.amax(time2))
         else:
             ans01['p3'] = -1
+        ans01['p4'] = int(calEnergy(grid1))
 
         result.append(ans01)
 
     return jsonify(result)
+
+def calEnergy(grid):
+    grid1 = np.copy(grid)
+    grid1[grid1==2] = 0
+    m, n = grid1.shape
+
+    healthies = np.argwhere(grid1==1)
+    healthies = [tuple(h.tolist()) for h in healthies]
+    # print(healthies)
+
+    e1 = 0
+    while len(healthies)>0:
+        energies = []
+        paths = []
+        for h in healthies:
+            e, p = findEnergy1(h, grid1, m, n)
+            energies.append(e)
+            paths.append(p)
+        e1 += np.amin(energies)
+        healthies.pop(np.argmin(energies))
+        for pt in paths[np.argmin(energies)]:
+            grid1[pt[0],pt[1]] = 3
+        # print(grid1)
+    # print(e1)
+    return e1
+
+
+def findEnergy1(h, grid1, m, n):
+    visited = {}
+    queue1 = [h]
+    visited[h] = (0,[h])
+    while len(queue1)>0:
+        h1 = queue1.pop(0)
+        i,j = h1[0],h1[1]
+        e = visited[h1][0]
+        path = visited[h1][1]
+        if not i-1<0:
+            if grid1[i-1,j] == 3:
+                break
+            if ((i-1,j) not in visited):
+                queue1.append((i-1,j))
+                visited[(i-1,j)] = (e+1,path+[(i-1,j)])
+        if not i+1>=m:
+            if grid1[i+1,j] == 3:
+                break
+            if ((i+1,j) not in visited):
+                queue1.append((i+1,j))
+                visited[(i+1,j)] = (e+1,path+[(i+1,j)])
+        if not j-1<0:
+            if grid1[i,j-1] == 3:
+                break
+            if ((i,j-1) not in visited):
+                queue1.append((i,j-1))
+                visited[(i,j-1)] = (e+1,path+[(i,j-1)])
+        if not j-1>=n:
+            if grid1[i,j+1] == 3:
+                break
+            if ((i,j+1) not in visited):
+                queue1.append((i,j+1))
+                visited[(i,j+1)] = (e+1,path+[(i,j+1)])
+    # print(visited)
+    return e, path
+
+
 
 def infection(grid, typeP):
     grid1 = np.array(grid, dtype=int)
@@ -58,8 +123,6 @@ def infection(grid, typeP):
                         ch1 = infectNeighbour1(grid1p5, time1, i, j, m, n, t)
                     elif typeP=='B':
                         ch1 = infectNeighbour2(grid1p5, time1, i, j, m, n, t)
-                    else:
-                        ch1 = infectNeighbour1(grid1p5, time1, i, j, m, n, t)
                     changes1 += ch1
         grid1 = grid1p5
         if changes1 == 0:
@@ -134,3 +197,13 @@ def infectNeighbour2(grid1p5, time1, i, j, m, n, t):
             time1[i+1,j+1] = t
             changes += 1
     return changes
+
+if __name__ == "__main__":
+    h = (2,0)
+    grid1 = np.array([
+      [0, 3, 1],
+      [0, 3, 3],
+      [1, 0, 0]
+    ])
+    calEnergy(grid1)
+    # findEnergy1(h, grid1, 3, 3)
