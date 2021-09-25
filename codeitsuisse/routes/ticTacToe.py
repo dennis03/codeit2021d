3b6bc8b56b0ccaa8bcc983aee2a19320aa26dd23
dennis03 <dennisleung03@gmail.com>
2021-09-25 11:54:09 +0800

    TTT01

---
+++ codeitsuisse/routes/ticTacToe.py
@@ -0,0 +1,37 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+import pprint
+import requests
+import sseclient
+
+logger = logging.getLogger(__name__)
+
+@app.route('/tic-tac-toe', methods=['POST'])
+def TTT():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+
+    battleId = data.get("battleId")
+    url = 'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/'
+    streamResponse = requests.get(url, params=battleId, stream=True)
+
+    avaActions = ['NW','N','NE','W','C','E','SW','S','SE']
+
+    client = sseclient.SSEClient(streamResponse)
+    for event in client.events():
+        # print("got a new event from server")
+        pprint.pprint(event.data)
+        
+
+
+    # result = inputValue * inputValue
+    # logging.info("My result :{}".format(result))
+    return 1
+
+
+
