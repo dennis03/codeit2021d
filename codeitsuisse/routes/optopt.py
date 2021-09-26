8910431f1d38ee475fd5e648ec0915ebc1a7f496
dennis03 <dennisleung03@gmail.com>
2021-09-26 08:49:47 +0800

    optopt

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -7,13 +7,13 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/options', methods=['POST'])
-def options():
+@app.route('/optopt', methods=['POST'])
+def optopt():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     options = data['options']
     views = data['view']
-    
+
     return json.dumps(1)
 
 
