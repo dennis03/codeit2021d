b7fb23fad838e40f9dbd545847a982c05122cb63
dennis03 <dennisleung03@gmail.com>
2021-09-26 09:53:39 +0800

    optopt

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -14,8 +14,8 @@ logger = logging.getLogger(__name__)
 def optopt():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    options = data['options']
-    views = data['view']
+    # options = data['options']
+    # views = data['view']
 
     # pdf1 = calPDF(views)
 
@@ -23,22 +23,22 @@ def optopt():
     result = [100]
     return jsonify(result)
 
-def calPDF(views):
-    means = [v['mean'] for v in views]
-    vars = [v['var'] for v in views]
-    mins = [v['min'] for v in views]
-    maxs = [v['max'] for v in views]
-    weights = [v['weight'] for v in views]
-
-    size = 10000
-    minA = np.amin(mins)
-    maxB = np.amax(maxs)
-    X = np.linspace(minA, maxB, size)
-    pdf1 = np.zeros(X.shape)
-    for k in np.arange(size):
-        x = X[k]
-        # for i in range(len(views)):
-        #     pdf1[k] +=
+# def calPDF(views):
+#     means = [v['mean'] for v in views]
+#     vars = [v['var'] for v in views]
+#     mins = [v['min'] for v in views]
+#     maxs = [v['max'] for v in views]
+#     weights = [v['weight'] for v in views]
+#
+#     size = 10000
+#     minA = np.amin(mins)
+#     maxB = np.amax(maxs)
+#     X = np.linspace(minA, maxB, size)
+#     pdf1 = np.zeros(X.shape)
+#     for k in np.arange(size):
+#         x = X[k]
+#         # for i in range(len(views)):
+#         #     pdf1[k] +=
 
 
 
