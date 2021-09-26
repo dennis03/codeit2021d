0c2a8f38ac87fa9a59bf6381eced18288f8b285f
dennis03 <dennisleung03@gmail.com>
2021-09-26 09:47:16 +0800

    optopt

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -5,6 +5,9 @@ from flask import request, jsonify
 
 from codeitsuisse import app
 
+import numpy as np
+import scipy
+
 logger = logging.getLogger(__name__)
 
 @app.route('/optopt', methods=['POST'])
@@ -14,7 +17,29 @@ def optopt():
     options = data['options']
     views = data['view']
 
-    return json.dumps(1)
+    pdf1 = calPDF(views)
+
+
+    result = [100]
+    return jsonify(result)
+
+def calPDF(views):
+    means = [v['mean'] for v in views]
+    vars = [v['var'] for v in views]
+    mins = [v['min'] for v in views]
+    maxs = [v['max'] for v in views]
+    weights = [v['weight'] for v in views]
+
+    size = 10000
+    minA = np.amin(mins)
+    maxB = np.amax(maxs)
+    X = np.linspace(minA, maxB, size)
+    pdf1 = np.zeros(X.shape)
+    for k in np.arange(size):
+        x = X[k]
+        # for i in range(len(views)):
+        #     pdf1[k] +=
+
 
 
 
